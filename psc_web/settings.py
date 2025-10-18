b825ee1276d864c956a70b582b0e327e7c8aa5a7
luth3r <aravindor@gmail.com>
2025-10-18 11:03:13 +0530

    Apps typo fixed

--- psc_web/settings.py
+++ psc_web/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assessment',
+    'communication',
+    'course',
+    'staff',
+    'students',
+    'users'
 ]
 
 MIDDLEWARE = [
