8587ec900760cac22abdf29a252dc9af9cd0b40c
luth3r <aravindor@gmail.com>
2025-12-15 18:44:22 +0530

    Function based view crud completed

--- course/urls.py
+++ course/urls.py
@@ -3,4 +3,6 @@ from course import views
 
 urlpatterns = [
     path('fbv/courses', views.courses),
+    path('fbv/courses/<int:id>', views.course_detail),
+    path('fbv/courses/<int:id>/<str:action>', views.course_actions),
 ]
--- course/views.py
+++ course/views.py
@@ -4,11 +4,72 @@ from rest_framework.response import Response
 from rest_framework import status
 from course import models
 from course import serializer
+from students.models import Student
+from students.serializer import StudentMinSerializer
 # Create your views here.
 
 
 @api_view(["GET", "POST"])
 def courses(request):
-    courses = models.Course.objects.all()
-    se = serializer.CourseSerializer(courses, many=True)
-    return Response(data=se.data, status=status.HTTP_200_OK)
+    if request.method == "GET":
+        courses = models.Course.objects.all()
+        se = serializer.CourseSerializer(courses, many=True)
+        return Response(data=se.data, status=status.HTTP_200_OK)
+    else:
+        se = serializer.CourseSerializer(data=request.data)
+        if not se.is_valid():
+            return Response(data=se.errors, status=status.HTTP_400_BAD_REQUEST)
+        obj = se.save()
+        _se = serializer.CourseSerializer(obj)
+        return Response(data=_se.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+def course_detail(request, id):
+    if request.method == "GET":
+        try:
+            model = models.Course.objects.get(id=id)
+            se = serializer.CourseSerializer(model)
+            return Response(data=se.data, status=status.HTTP_200_OK)
+        except models.Course.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == "PUT":
+        try:
+            model = models.Course.objects.get(id=id)
+            se = serializer.CourseSerializer(model, data=request.data)
+            if not se.is_valid():
+                return Response(data=se.errors, status=status.HTTP_400_BAD_REQUEST)
+            se.save()
+            return Response(data=se.data, status=status.HTTP_200_OK)
+        except models.Course.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == "PATCH":
+        try:
+            model = models.Course.objects.get(id=id)
+            se = serializer.CourseSerializer(
+                model, data=request.data, partial=True)
+            if not se.is_valid():
+                return Response(data=se.errors, status=status.HTTP_400_BAD_REQUEST)
+            se.save()
+            return Response(data=se.data, status=status.HTTP_200_OK)
+        except models.Course.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == "DELETE":
+        try:
+            model = models.Course.objects.get(id=id)
+            model.delete()
+            return Response(data=se.data, status=status.HTTP_204_NO_CONTENT)
+        except models.Course.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["GET"])
+def course_actions(request, id, action):
+    if action == "students":
+        students = Student.objects.filter(enrollment__course__id=id)
+        se = StudentMinSerializer(students, many=True)
+        return Response(data=se.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_200_OK)
